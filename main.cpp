750537b5d936f0d627a43ba0a135fdaecce4037a
yuyz <yuyz.ccnu@live.com>
2022-03-09 00:01:52 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <SFML/Window.hpp>
+
+int main(int argc, char** argv)
+{
+    sf::Window Window(sf::VideoMode(800, 600), L"测试");
+    while (Window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (Window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                Window.close();
+        }
+    }
+
+    return 0;
+}
